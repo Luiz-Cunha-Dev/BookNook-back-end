02da4443f19927d271bec37e8f3e49bf8bcc53b5
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2023-04-14 20:13:36 -0300

    fix: json ranking route

--- src/services/ranking.service.ts
+++ src/services/ranking.service.ts
@@ -11,12 +11,12 @@ export async function getGeneralRanking() {
 
         for (let i = 0; i < highestRated.length; i++) {
             const entertainment = await rankingRepository.getEntertainmentById(highestRated[i].entertainmentId);
-            highestRatedList.push(entertainment);
+            highestRatedList.push({...entertainment, _avg: highestRated[i]._avg});
         }
 
         for (let i = 0; i < mostViewed.length; i++) {
             const entertainment = await rankingRepository.getEntertainmentById(mostViewed[i].entertainmentId);
-            mostViewedList.push(entertainment);
+            mostViewedList.push({...entertainment, _count: mostViewed[i]._count});
         }
 
         return {highestRated: highestRatedList, mostViewed: mostViewedList};
